26ecf7c99ddc5e8345096b64e5a4c1d67bb2b4df
Synchromatic <synchromatic.github@gmail.com>
2023-06-04 12:26:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 using namespace std;
+using namespace filesystem;
 
 class Trim {
 private:
@@ -19,14 +20,14 @@ public:
     void right_trim() {
         string str = this->str;
         const char *spaces = this->spaces;
-        char whitespace_chars = str.find_last_not_of(spaces) + 1;
+        size_t whitespace_chars = str.find_last_not_of(spaces) + 1;
         this->str = str.erase(whitespace_chars);
     }
 
     void left_trim() {
         string str = this->str;
         const char *spaces = this->spaces;
-        char whitespace_chars = str.find_first_not_of(spaces);
+        size_t whitespace_chars = str.find_first_not_of(spaces);
         this->str = str.erase(0, whitespace_chars);
     }
 
@@ -38,17 +39,178 @@ public:
 
 class FileSearch {
 private:
-    optional<filesystem::path> root;
+    optional<path> root;
     vector<string> include_filenames;
     vector<string> include_exts;
-    vector<filesystem::path> exclude_dirs;
+    vector<path> exclude_dirs;
 
-public:
-    FileSearch() {
+    string format_extension(string &ext) {
+        Trim(ext).trim();
+        transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
+
+        if (!ext.empty() && ext[0] != '.') {
+            ext.insert(0, ".");
+        }
+        return ext;
     }
 
+    bool get_filter_validation(path &fpath) {
+        bool is_included_filename = this->is_included_filename(fpath);
+        bool is_included_extension = this->is_included_extension(fpath);
+        bool filter_validation = is_included_filename && is_included_extension;
+        return filter_validation;
+    }
+
+    optional<path> get_canonical_path(path &entry) {
+        try {
+            path path = filesystem::canonical(entry);
+            return path;
+        } catch (filesystem_error &e) {
+            printf("File Inaccessible: [%s]\n\n", entry.string().c_str());
+            return nullopt;
+        }
+    }
+
+    optional<directory_iterator> get_directory_entries(path &root) {
+        try {
+            directory_iterator entries = directory_iterator(root);
+            return entries;
+        } catch (filesystem_error &e) {
+            printf("Directory Inaccessible: [%s]\n\n", root.string().c_str());
+            return nullopt;
+        }
+    }
+
+    path get_abs_path() {
+        return filesystem::current_path();
+    }
+
+    path get_root_path() {
+        if (this->root.has_value()) {
+            return this->root.value();
+        }
+        return get_abs_path();
+    }
+
+    bool is_same_directory(path &file, path &dir) {
+        if (exists(dir)) {
+            path dir_canon = canonical(dir);
+            path file_canon = canonical(file);
+
+            while (file_canon != file_canon.root_path()) {
+                if (file_canon == dir_canon) {
+                    return true;
+                }
+                file_canon = file_canon.parent_path();
+            }
+        }
+        return false;
+    }
+
+    bool is_included_filename(path &fpath) {
+        if (this->include_filenames.empty()) {
+            return true;
+        }
+
+        string file_stem = fpath.stem().string();
+        for (string file_name : this->include_filenames) {
+            if (file_name == file_stem) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool is_included_extension(path &fpath) {
+        if (this->include_exts.empty()) {
+            return true;
+        }
+
+        for (string ext : this->include_exts) {
+            ext = this->format_extension(ext);
+            string file_ext = fpath.extension().string();
+            file_ext = this->format_extension(file_ext);
+
+            if (file_ext == ext) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool is_excluded_directory(path &fpath) {
+        if (this->exclude_dirs.empty()) {
+            return false;
+        }
+
+        if (!fpath.is_absolute()) {
+            optional<path> fpath_op = this->get_canonical_path(fpath);
+            if (!fpath_op.has_value()) {
+                return false;
+            }
+            fpath = fpath_op.value();
+        }
+
+        for (path dir : this->exclude_dirs) {
+            bool is_same_directory = this->is_same_directory(fpath, dir);
+            if (is_same_directory) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void handle_file(path &fpath, vector<path> &files) {
+        bool filter_validation = this->get_filter_validation(fpath);
+        bool contains_path = find(files.begin(), files.end(), fpath) != files.end();
+
+        if (!contains_path && filter_validation) {
+            files.push_back(fpath);
+        }
+    }
+
+    void handle_folder(path &fpath, vector<path> &roots, vector<path> &files) {
+        bool contains_path = find(roots.begin(), roots.end(), fpath) != roots.end();
+
+        if (!contains_path) {
+            roots.push_back(fpath);
+            this->search(fpath, roots, files);
+        }
+    }
+
+    void walker(directory_iterator &entries, vector<path> &roots, vector<path> &files) {
+        for (path entry : entries) {
+            optional<path> op_fpath = this->get_canonical_path(entry);
+            if (!op_fpath.has_value()) {
+                continue;
+            }
+            path fpath = op_fpath.value();
+
+            if (filesystem::is_regular_file(fpath)) {
+                this->handle_file(fpath, files);
+
+            } else if (filesystem::is_directory(fpath)) {
+                this->handle_folder(fpath, roots, files);
+            }
+        }
+    }
+
+    void search(path &root, vector<path> &roots, vector<path> &files) {
+        if (this->is_excluded_directory(root)) {
+            return;
+        }
+
+        optional<directory_iterator> op_entries = directory_iterator(root);
+
+        if (op_entries.has_value()) {
+            directory_iterator entries = op_entries.value();
+            this->walker(entries, roots, files);
+        }
+    }
+
+public:
     void set_root(string &root) {
-        this->root = filesystem::path(root);
+        this->root = path(root);
     }
 
     void set_include_filenames(vector<string> &filenames) {
@@ -72,157 +234,29 @@ public:
     }
 
     void set_exclude_directories(vector<string> &dirs) {
-        vector<filesystem::path> exclude_dirs;
-        for (filesystem::path dir : dirs) {
-            exclude_dirs.push_back(filesystem::path(dir));
+        vector<path> exclude_dirs;
+        for (path dir : dirs) {
+            exclude_dirs.push_back(path(dir));
         }
         this->exclude_dirs = exclude_dirs;
     }
 
-    vector<filesystem::path> search_files() {
-        vector<filesystem::path> roots;
-        vector<filesystem::path> files;
+    vector<path> search_files() {
+        vector<path> roots;
+        vector<path> files;
 
-        filesystem::path root = this->get_root_path();
-        this->recursive_search(root, roots, files);
+        path root = this->get_root_path();
+        this->search(root, roots, files);
         return files;
-    }
-
-    string format_extension(string &ext) {
-        Trim(ext).trim();
-        transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
-
-        if (!ext.empty() && ext[0] != '.') {
-            ext.insert(0, ".");
-        }
-        return ext;
-    }
-
-    bool is_same_directory(filesystem::path &file, filesystem::path &dir) {
-        if (filesystem::exists(dir)) {
-            filesystem::path dir_canon = filesystem::canonical(dir);
-            filesystem::path file_canon = filesystem::canonical(file);
-
-            while (file_canon.has_parent_path()) {
-                file_canon = file_canon.parent_path();
-                if (file_canon == dir_canon) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    bool is_included_filename(filesystem::path &file_path) {
-        if (this->include_filenames.empty()) {
-            return true;
-        }
-
-        string file_stem = file_path.stem().string();
-        for (string file_name : this->include_filenames) {
-            if (file_name == file_stem) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool is_included_extension(filesystem::path &file_path) {
-        if (this->include_exts.empty()) {
-            return true;
-        }
-
-        for (string ext : this->include_exts) {
-            ext = this->format_extension(ext);
-            string file_ext = file_path.extension().string();
-            file_ext = this->format_extension(file_ext);
-
-            if (file_ext == ext) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool is_excluded_directory(filesystem::path &file_path) {
-        if (this->exclude_dirs.empty()) {
-            return false;
-        }
-
-        for (filesystem::path dir : this->exclude_dirs) {
-            bool is_same_directory = this->is_same_directory(file_path, dir);
-            if (is_same_directory) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool filter_pass(filesystem::path &file_path) {
-        bool is_included_filename = this->is_included_filename(file_path);
-        bool is_included_extension = this->is_included_extension(file_path);
-        bool is_excluded_directory = this->is_excluded_directory(file_path);
-
-        bool filters = is_included_filename && is_included_extension && !is_excluded_directory;
-        return filters;
-    }
-
-    void handle_file(filesystem::path &path, vector<filesystem::path> &files) {
-        bool filter_pass = this->filter_pass(path);
-        bool contains_path = find(files.begin(), files.end(), path) != files.end();
-
-        if (!contains_path && filter_pass) {
-            files.push_back(path);
-        }
-    }
-
-    void handle_folder(filesystem::path &path, vector<filesystem::path> &roots, vector<filesystem::path> &files) {
-        bool contains_path = find(roots.begin(), roots.end(), path) != roots.end();
-
-        if (!contains_path) {
-            roots.push_back(path);
-            this->recursive_search(path, roots, files);
-        }
-    }
-
-    void recursive_search(filesystem::path &root, vector<filesystem::path> &roots, vector<filesystem::path> &files) {
-        try {
-            filesystem::directory_iterator entries = filesystem::directory_iterator(root);
-
-            for (filesystem::path entry : entries) {
-                filesystem::path path = filesystem::canonical(entry);
-
-                if (filesystem::is_regular_file(path)) {
-                    this->handle_file(path, files);
-
-                } else if (filesystem::is_directory(path)) {
-                    this->handle_folder(path, roots, files);
-                }
-            }
-
-        } catch (filesystem::filesystem_error &e) {
-            printf("Directory Inaccessible: [%s]\n", root.string().c_str());
-        }
-    }
-
-    filesystem::path get_abs_path() {
-        return filesystem::current_path();
-    }
-
-    filesystem::path get_root_path() {
-        if (this->root.has_value()) {
-            return this->root.value();
-        }
-        return get_abs_path();
     }
 };
 
 int main() {
     FileSearch file_search = FileSearch();
 
-    string root = "./";
+    string root = "C:/Users/shady/Downloads";
     vector<string> include_filenames = {};
-    vector<string> include_exts = {};
+    vector<string> include_exts = {".mp3"};
     vector<string> exclude_dirs = {};
 
     file_search.set_root(root);
@@ -230,9 +264,9 @@ int main() {
     file_search.set_include_extensions(include_exts);
     file_search.set_exclude_directories(exclude_dirs);
 
-    vector<filesystem::path> files = file_search.search_files();
+    vector<path> files = file_search.search_files();
 
-    for (filesystem::path file : files) {
+    for (path file : files) {
         printf("[%s]\n", file.string().c_str());
     }
 
